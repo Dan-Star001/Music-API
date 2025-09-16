1aad62fb9e01652049a836802fee072600a4abbf
Dan-Star001 <adedayodaniel1711@gmail.com>
2025-09-16 15:38:25 +0100

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const songRoute = require('./routes/song.route')
 const dotenv = require('dotenv')
 dotenv.config();
 
 
+
+// Use the cors middleware
+app.use(cors());
 
 
 app.use('/', songRoute)
